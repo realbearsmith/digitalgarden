81b72d9330446107f3d18c566baa9400de56eaf6
Jared Smith <smithjaredtaylor@gmail.com>
2021-12-08 18:46:43 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -49,7 +49,7 @@ module.exports = function (eleventyConfig) {
     });
 
     eleventyConfig.addFilter("isValidDate", (date) => {
-        date !== "" && fromISO(date).isValid()
+        date !== "" /*&& fromISO(date).isValid()*/
     });
 
     return {
